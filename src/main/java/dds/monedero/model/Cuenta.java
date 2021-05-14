3e7397fa44b6d460922410306cd1ba22d523e4ad
FrancoPaesani <62572002+FrancoPaesani@users.noreply.github.com>
2021-05-14 12:27:23 -0300

    agrego tests

--- src/main/java/dds/monedero/model/Cuenta.java
+++ src/main/java/dds/monedero/model/Cuenta.java
@@ -8,47 +8,55 @@ import dds.monedero.exceptions.SaldoMenorException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class Cuenta {
 
   private double saldo = 0;
+  private double limiteDiarioExtraccion;
+  private int cantDepositosMaximos;
   private List<Movimiento> movimientos = new ArrayList<>();
 
-  public Cuenta() { saldo = 0; }
-
-  public Cuenta(double montoInicial) { saldo = montoInicial; }
+  public Cuenta(Double montoInicial, double limiteDiarioExtraccion, int cantDepositosMaximos) {
+    if(montoInicial != null) {
+      this.saldo = (double) montoInicial;
+    }
+    this.limiteDiarioExtraccion = Objects.requireNonNull(limiteDiarioExtraccion,"Se  requiere un limite diario de extraccion");
+    this.cantDepositosMaximos = Objects.requireNonNull(cantDepositosMaximos,"Se  requiere un limite diario de depositos.");
+  }
 
   public void setMovimientos(List<Movimiento> movimientos) {
     this.movimientos = movimientos;
   }
 
-  public void poner(double cuanto) {
-    validarMontoNoNegativo(cuanto);
-    validarCantidadDepositosDiarios(3);
+  public void poner(double monto) {
+    validarMontoNoNegativo(monto);
+    validarCantidadDepositosDiarios();
 
-    Movimiento movimientoActual = new Movimiento(LocalDate.now(), cuanto, Deposito.getINSTANCE());
+    Movimiento movimientoActual = new Movimiento(LocalDate.now(), monto, Deposito.getINSTANCE());
     this.agregarMovimiento(movimientoActual);
-    this.saldo = movimientoActual.calcularValor(saldo, cuanto);
+    this.saldo = movimientoActual.calcularValor(saldo, monto);
   }
 
-  public void sacar(double cuanto) {
-    validarMontoNoNegativo(cuanto);
-    validarSaldoSuficiente(getSaldo(), cuanto);
-    validarLimiteDiario(getMontoExtraidoA(LocalDate.now()), cuanto, 1000);
+  public void sacar(double monto) {
+    validarMontoNoNegativo(monto);
+    validarLimiteDiario(getMontoExtraidoA(LocalDate.now()), monto);
+    validarSaldoSuficiente(getSaldo(), monto);
 
-    Movimiento movimientoActual = new Movimiento(LocalDate.now(), cuanto, Extraccion.getINSTANCE());
+    Movimiento movimientoActual = new Movimiento(LocalDate.now(), monto, Extraccion.getINSTANCE());
     this.agregarMovimiento(movimientoActual);
-    this.saldo = movimientoActual.calcularValor(saldo, cuanto);
+    this.saldo = movimientoActual.calcularValor(saldo, monto);
   }
   private void validarSaldoSuficiente(double saldo, double monto) {
     if (saldo - monto < 0) {
       throw new SaldoMenorException("No puede sacar mas de " + getSaldo() + " $");
     }
   }
-  private void validarLimiteDiario(double montoExtraidoHoy, double montoExtraccion, double limite) {
-    if (montoExtraccion > limite - montoExtraidoHoy) {
-      throw new MaximoExtraccionDiarioException("No puede extraer mas de $ " + limite
-              + " diarios, extracción restante en el día de hoy: " + (limite - montoExtraidoHoy));
+  private void validarLimiteDiario(double montoExtraidoHoy, double montoExtraccion) {
+    if (montoExtraccion > limiteDiarioExtraccion - montoExtraidoHoy) {
+      throw new MaximoExtraccionDiarioException("No puede extraer mas de $ " + limiteDiarioExtraccion
+              + " diarios, extracción restante en el día de hoy: " + (limiteDiarioExtraccion - montoExtraidoHoy));
     }
   }
   private void validarMontoNoNegativo(double monto) {
@@ -57,8 +65,8 @@ public class Cuenta {
     }
   }
 
-  private void validarCantidadDepositosDiarios(Integer cantDepositosMaximos) {
-    if (getMovimientos().stream().filter(movimiento -> movimiento.isDeposito()).count() >= cantDepositosMaximos) {
+  private void validarCantidadDepositosDiarios() {
+    if (this.cantDepositosEnElDia(LocalDate.now()) >= cantDepositosMaximos) {
       throw new MaximaCantidadDepositosException("Ya excedio los " + cantDepositosMaximos + " depositos diarios");
     }
   }
@@ -71,6 +79,11 @@ public class Cuenta {
         .filter(movimiento -> movimiento.fueExtraido(fecha))
         .mapToDouble(Movimiento::getMonto)
         .sum();
+  }
+
+  public int cantDepositosEnElDia(LocalDate fecha) {
+    return getMovimientos().stream()
+            .filter(movimiento -> movimiento.fueDepositado(fecha)).collect(Collectors.toList()).size();
   }
 
   public List<Movimiento> getMovimientos() {
--- src/test/java/dds/monedero/model/MonederoTest.java
+++ src/test/java/dds/monedero/model/MonederoTest.java
@@ -4,65 +4,86 @@ import dds.monedero.exceptions.MaximaCantidadDepositosException;
 import dds.monedero.exceptions.MaximoExtraccionDiarioException;
 import dds.monedero.exceptions.MontoNegativoException;
 import dds.monedero.exceptions.SaldoMenorException;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class MonederoTest {
-  private Cuenta cuenta;
+  private Cuenta cuentaJorge;
+  private Cuenta cuentaManuel;
+  private Cuenta cuentaJosefa;
+  private Cuenta cuentaDaniel;
 
   @BeforeEach
   void init() {
-    cuenta = new Cuenta();
+    cuentaJorge = new Cuenta(new Double(1000), 500,3);
+    cuentaManuel = new Cuenta(new Double(1000), 2000,2);
+    cuentaJosefa = new Cuenta(new Double(2000),1000,1);
+    cuentaDaniel = new Cuenta(new Double(5000), 2500,2);
   }
 
   @Test
-  void Poner() {
-    cuenta.poner(1500);
+  void unDepositode100EnCuentaQueTiene1000Suma1100() {
+    assertEquals(Deposito.getINSTANCE().calcularNuevoSaldo(1000, 100), 1100);
   }
 
   @Test
-  void PonerMontoNegativo() {
-    assertThrows(MontoNegativoException.class, () -> cuenta.poner(-1500));
+  void unaExtraccionDe100EnCuentaQueTiene1000Es900() {
+    assertEquals(Extraccion.getINSTANCE().calcularNuevoSaldo(1000, 100), 900);
   }
 
   @Test
-  void TresDepositos() {
-    cuenta.poner(1500);
-    cuenta.poner(456);
-    cuenta.poner(1900);
+  void DepositoDe500EnCuentaDe2000Tiene2500() {
+    cuentaJosefa.poner(500);
+    assertEquals(cuentaJosefa.getSaldo(),2500,0);
   }
 
   @Test
-  void MasDeTresDepositos() {
-    assertThrows(MaximaCantidadDepositosException.class, () -> {
-          cuenta.poner(1500);
-          cuenta.poner(456);
-          cuenta.poner(1900);
-          cuenta.poner(245);
-    });
+  void MasDetresDepositosEnCuentaCon3DepositosMaximosNoSePuede() {
+    cuentaJorge.poner(100);
+    cuentaJorge.poner(200);
+    cuentaJorge.poner(200);
+    assertThrows(MaximaCantidadDepositosException.class,() -> cuentaJorge.poner(100));
   }
 
   @Test
-  void ExtraerMasQueElSaldo() {
-    assertThrows(SaldoMenorException.class, () -> {
-          cuenta.setSaldo(90);
-          cuenta.sacar(1001);
-    });
+  void PuedoTresDepositosEnCuentaCon3DepositosMaximos() {
+    cuentaJorge.poner(100);
+    cuentaJorge.poner(200);
+    assertEquals(cuentaJorge.getMovimientos().size(),2);
   }
 
   @Test
-  public void ExtraerMasDe1000() {
-    assertThrows(MaximoExtraccionDiarioException.class, () -> {
-      cuenta.setSaldo(5000);
-      cuenta.sacar(1001);
-    });
+  void DosExtraccionesDe500Y1DepositoDe1000CuentaTiene3Operaciones() {
+    cuentaJosefa.sacar(500);
+    cuentaJosefa.sacar(500);
+    cuentaJosefa.poner(1000);
+    assertEquals(cuentaJosefa.getMovimientos().size(),3);
   }
 
   @Test
-  public void ExtraerMontoNegativo() {
-    assertThrows(MontoNegativoException.class, () -> cuenta.sacar(-500));
+  void NingunaCuentaPuedeDepositarMontoNegativo() {
+    assertThrows(MontoNegativoException.class, () -> cuentaManuel.poner(-1));
+  }
+
+  @Test
+  public void NingunaCuentaPuedeExtraerMontoNegativo() {
+    assertThrows(MontoNegativoException.class, () -> cuentaDaniel.sacar(-500));
+  }
+
+  @Test
+  void CuentaQueTieneMonto1000NoSePuedeExtraerMasDe1000() {
+    assertThrows(SaldoMenorException.class, () -> cuentaManuel.sacar(1100));
+  }
+
+  @Test
+  void ExtraccionDiariaNoPuedo5000EnCuentaConLimiteExtraccion2500() {
+    assertThrows(MaximoExtraccionDiarioException.class, () -> cuentaDaniel.sacar(5000));
   }
 
 }
