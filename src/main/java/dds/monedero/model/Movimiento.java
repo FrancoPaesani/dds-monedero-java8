516acaab2b7e07edf3a66cc92329d06b219b86c9
FrancoPaesani <62572002+FrancoPaesani@users.noreply.github.com>
2021-05-14 09:58:09 -0300

    Code Smell 5: Elimino método isExtraccion()

--- src/main/java/dds/monedero/model/Movimiento.java
+++ src/main/java/dds/monedero/model/Movimiento.java
@@ -7,7 +7,7 @@ public class Movimiento {
   //En ningún lenguaje de programación usen jamás doubles para modelar dinero en el mundo real
   //siempre usen numeros de precision arbitraria, como BigDecimal en Java y similares
   private double monto;
-  private boolean esDeposito; //delegarlo con un strategy
+  private boolean esDeposito;
 
   public Movimiento(LocalDate fecha, double monto, boolean esDeposito) {
     this.fecha = fecha;
@@ -28,7 +28,7 @@ public class Movimiento {
   }
 
   public boolean fueExtraido(LocalDate fecha) {
-    return isExtraccion() && esDeLaFecha(fecha);
+    return !isDeposito() && esDeLaFecha(fecha);
   }
 
   public boolean esDeLaFecha(LocalDate fecha) {
@@ -37,10 +37,6 @@ public class Movimiento {
 
   public boolean isDeposito() {
     return esDeposito;
-  }
-
-  public boolean isExtraccion() {
-    return !esDeposito;
   }
 
   public void agregateA(Cuenta cuenta) {
